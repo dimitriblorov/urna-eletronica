ea0a0bc4fee32626a833df29a15d876fe5be7d44
dimitriblorov <dimitriblorov@gmail.com>
2021-10-28 21:06:11 -0300

    Urna eletronica

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero:'75111',
+                nome: 'Geovanildo Silva',
+                partido: 'PKJ',
+                fotos: [
+                    {url:'38111.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero:'85588',
+                nome: 'Rosivaldo Pereira',
+                partido: 'PUP',
+                fotos: [
+                    {url:'77222.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '86',
+                nome: 'Periovaldo Santos',
+                partido: 'PXZ',
+                vice: 'Marinho da Luz',
+                fotos:[
+                    {url:'99.jpg', legenda: 'Prefeito'},
+                    {url: '99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '89',
+                nome: 'Silvinho da Paz',
+                partido: 'PQL',
+                vice: 'Lucinho Machado',
+                fotos:[
+                    {url:'84.jpg', legenda: 'Prefeito'},
+                    {url: '84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]   
+            },
+        ]
+    }
+];
